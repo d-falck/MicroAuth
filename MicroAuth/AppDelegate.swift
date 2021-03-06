0f7af3da330d557b7d711cd61d23695c8f9f6bd2
Damon Falck <damon.falck@worc.ox.ac.uk>
2021-03-06 14:35:37 +0000

    Initial Commit

---
+++ MicroAuth/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MicroAuth
+//
+//  Created by Damon Falck on 06/03/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MicroAuth/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MicroAuth
+//
+//  Created by Damon Falck on 06/03/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
